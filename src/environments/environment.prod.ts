65660729ffae43e6869107d95161476b0c3c11d7
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-01-06 00:05:08 +0200

    chore(environments): switch to production backend URLs
    - Update production environment to use https://backend.gorhom.com API
    - Update development environment to use https://backend.gorhom.com API
    - Comment out local test server URLs (http://gorhom.test) for both environments
    - Align both environment configurations to use the same production backend
    - This ensures consistent API endpoints across development and production builds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // backEndUrl: 'https://backend.gorhom.com/api',
-  // imgUrl: 'https://backend.gorhom.com/',
-  backEndUrl: 'http://gorhom.test/api',
-  imgUrl: 'http://gorhom.test/',
+  backEndUrl: 'https://backend.gorhom.com/api',
+  imgUrl: 'https://backend.gorhom.com/',
+  // backEndUrl: 'http://gorhom.test/api',
+  // imgUrl: 'http://gorhom.test/',
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // backEndUrl: 'https://backend.gorhom.com/api',
-  // imgUrl: 'https://backend.gorhom.com/',
-    backEndUrl: 'http://gorhom.test/api',
-  imgUrl: 'http://gorhom.test/',
+  backEndUrl: 'https://backend.gorhom.com/api',
+  imgUrl: 'https://backend.gorhom.com/',
+  //   backEndUrl: 'http://gorhom.test/api',
+  // imgUrl: 'http://gorhom.test/',
 };
